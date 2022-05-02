409ed4448298e307c32604d01e9ac2d7b02d3466
Jerry <1020478666@qq.com>
2022-05-02 21:04:12 +0800

    add guava cache

--- src/main/java/com/jiangnan/service/impl/UserServiceImpl.java
+++ src/main/java/com/jiangnan/service/impl/UserServiceImpl.java
@@ -1,10 +1,17 @@
 package com.jiangnan.service.impl;
 
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.jiangnan.domain.User;
-import com.jiangnan.service.UserService;
 import com.jiangnan.mapper.UserMapper;
+import com.jiangnan.service.UserService;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 /**
 * @author jerry
@@ -15,6 +22,20 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     implements UserService{
 
+    Cache<String, String> cache = CacheBuilder.newBuilder().maximumSize(1000).expireAfterAccess(30, TimeUnit.SECONDS).build();
+    LoadingCache<String, String> loadingCache = CacheBuilder.newBuilder().maximumSize(1000).expireAfterAccess(30, TimeUnit.SECONDS)
+            .build(CacheLoader.from(this::getName));
+
+    public String getName(String s) {
+        String cacheResult = cache.getIfPresent(s);
+        if (StringUtils.isNotBlank(cacheResult)) {
+            return cacheResult;
+        } else {
+            String result = "cache" + s;
+            cache.put(s, result);
+            return "Tom" + s;
+        }
+    }
 }
 
 
