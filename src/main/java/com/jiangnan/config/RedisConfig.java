5fcfd1fec3afad4e82bbe06c8db696ba043d2f79
Jerry <1020478666@qq.com>
2022-05-29 21:05:33 +0800

    add redission

---
+++ src/main/java/com/jiangnan/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package com.jiangnan.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedissonClient getRedissionClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://localhost:6379")
+                .setPassword("123456");
+
+        RedissonClient redisson = Redisson.create(config);
+        return redisson;
+    }
+}
--- src/main/java/com/jiangnan/service/impl/MyServiceImpl.java
+++ src/main/java/com/jiangnan/service/impl/MyServiceImpl.java
@@ -5,9 +5,14 @@ import com.jiangnan.domain.User;
 import com.jiangnan.mapper.UserMapper;
 import com.jiangnan.service.MyService;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RReadWriteLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 //@Service
 @Slf4j
@@ -17,10 +22,24 @@ public class MyServiceImpl implements MyService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private RedissonClient redission;
+
     @Override
-//    @Cacheable(value = "USER", keyGenerator = "userGenerator")
+    @Cacheable(value = "USER", keyGenerator = "userGenerator")
     public User getUser(int id) {
         log.info("getUser + id: " + id);
         return userMapper.selectById(id);
+    }
+
+    public void testLock() throws InterruptedException {
+//        RReadWriteLock readWriteLock = redission.getReadWriteLock("myLock");
+//        RLock readLock = readWriteLock.readLock();
+//        RLock writeLock = readWriteLock.writeLock();
+        RLock rLock = redission.getLock("lock");
+        boolean lock = rLock.tryLock(100, 10, TimeUnit.SECONDS);
+        if (lock) {
+            System.out.println("get lock!");
+        }
     }
 }
